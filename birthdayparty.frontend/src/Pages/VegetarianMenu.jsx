68914d4d2cc0f7378a8c437caf053e5b9f0885ff
MinnCoww <cotmse172146@fpt.edu.vn>
2024-03-09 14:17:02 +0700

    update UI

--- birthdayparty.frontend/src/Pages/VegetarianMenu.jsx
+++ birthdayparty.frontend/src/Pages/VegetarianMenu.jsx
@@ -1,14 +1,23 @@
 import React, {Fragment} from 'react'
 import PageHeader from "../Components/PageHeader";
-
+import "../assets/css/kidsMenu.css";
+import {Link} from "react-router-dom";
 export default function VegetarianMenu() {
 
-  return (
-      <Fragment>
-        <PageHeader title={"Vegetarian Menu"}/>
-        <div>
-          VegetarianMenu
-        </div>
-      </Fragment>
-  )
+    return (
+        <Fragment>
+            <PageHeader title={"Vegetarian"}/>
+            <div className="menu">
+                <h1>KidsMenu</h1>
+                <ul>
+                    <li>Pizza</li>
+                    <li>Hamburgers</li>
+                    <li>Snacks</li>
+                    <li>Soft Drinks</li>
+                    <li>Fruit Drinks</li>
+                </ul>
+                <Link to="/booking" className="booking-btn">Booking Now</Link>
+            </div>
+        </Fragment>
+    )
 }
